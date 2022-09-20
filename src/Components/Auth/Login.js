27b9c80bdcccf1b27a8166f8daf783c337c5c58d
martiN <78382341+martynosa@users.noreply.github.com>
2022-09-20 23:42:29 +0300

    login and register design

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,5 +1,46 @@
+import { Link } from 'react-router-dom';
+
+import classes from './Auth.module.css';
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+  };
+
+  return (
+    <>
+      <form className={classes.form}>
+        <div className={`${classes.inputGroup} mb-12`}>
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" name="email" />
+        </div>
+
+        <div className={`${classes.inputGroup} mb-12`}>
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password" name="password" />
+        </div>
+
+        <div className={`${classes.linkGroup} mb-24`}>
+          <p>Don't have an account?</p>
+          <Link
+            to="/register"
+            className={`${classes.link} ${classes.yellowLink}`}
+          >
+            Register&nbsp;<ion-icon name="arrow-round-forward"></ion-icon>
+          </Link>
+        </div>
+
+        <button
+          type="submit"
+          onClick={onSubmitHandler}
+          className={`${classes.btn} ${classes.purpleBtn}`}
+        >
+          Log in
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,5 +1,53 @@
+import { Link } from 'react-router-dom';
+
+import classes from './Auth.module.css';
+
 const Register = () => {
-  return <h1>Register</h1>;
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+  };
+
+  return (
+    <>
+      <form className={classes.form}>
+        <div className={`${classes.inputGroup} mb-12`}>
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" name="email" />
+        </div>
+
+        <div className={`${classes.inputGroup} mb-12`}>
+          <label htmlFor="fullName">Name</label>
+          <input type="text" id="fullName" name="fullName" />
+        </div>
+
+        <div className={`${classes.inputGroup} mb-12`}>
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password" name="password" />
+        </div>
+
+        <div className={`${classes.inputGroup} mb-12`}>
+          <label htmlFor="rePassword">Repeat Password</label>
+          <input type="text" id="rePassword" name="rePassword" />
+        </div>
+
+        <div className={`${classes.linkGroup} mb-24`}>
+          <p>Already have an account?</p>
+          <Link to="/login" className={`${classes.link} ${classes.purpleLink}`}>
+            <ion-icon name="arrow-round-back"></ion-icon>&nbsp;Login
+          </Link>
+        </div>
+
+        <button
+          type="submit"
+          onClick={onSubmitHandler}
+          className={`${classes.btn} ${classes.yellowBtn}`}
+        >
+          Register
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
