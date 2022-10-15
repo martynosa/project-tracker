41abf1002c6c44bb7ddb96ef9e00c597e163204b
martiN <78382341+martynosa@users.noreply.github.com>
2022-10-15 19:06:34 +0300

    added input group for name and textarea in create page

--- src/Components/Common/Textarea.js
+++ src/Components/Common/Textarea.js
@@ -1,27 +1,24 @@
 import { useId } from 'react';
+import classes from './Textarea.module.css';
 
-import classes from './InputGroup.module.css';
-
-const InputGroup = ({ label, type, onChangeHandler, error, value }) => {
+const Textarea = ({ label, onChangeHandler, error, value }) => {
   const inputId = useId();
 
-  const inputType = type || 'text';
   const errorStatus = error?.status || false;
   const errorMessage = error?.message || '';
 
   return (
-    <div className={`${classes.inputGroup} mb-24`}>
+    <div className={`${classes.textAreaGroup} mb-24`}>
       <label htmlFor={inputId}>{label}</label>
-      <input
+      <textarea
         id={inputId}
-        type={inputType}
         onChange={onChangeHandler}
         className={errorStatus ? classes.errorInput : undefined}
         value={value}
-      />
+      ></textarea>
       {errorStatus && <p className={classes.errorMessage}>{errorMessage}</p>}
     </div>
   );
 };
 
-export default InputGroup;
+export default Textarea;
