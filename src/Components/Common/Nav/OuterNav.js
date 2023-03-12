c8d8e50e6cf783d6076c78ecc71a0dfd6c6d9952
martiN <78382341+martynosa@users.noreply.github.com>
2023-03-12 17:26:44 +0200

    removed links to other projects

--- src/Components/Common/Nav/OuterNav.js
+++ src/Components/Common/Nav/OuterNav.js
@@ -32,33 +32,7 @@ const OuterNav = () => {
           <ion-icon name="logo-github"></ion-icon>
           Github
         </a>
-        <a
-          className={classes.link}
-          href="https://martynosa-react-weather.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <ion-icon name="partly-sunny"></ion-icon>
-          Weather
-        </a>
-        <a
-          className={classes.link}
-          href="https://martynosa-sharingan.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <ion-icon name="eye"></ion-icon>
-          Sharingan
-        </a>
-        <a
-          className={classes.link}
-          href="https://martynosa-omnifood.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <ion-icon name="pizza"></ion-icon>
-          Omnifood
-        </a>
+
         <div className={classes.theme}>
           <ThemeButton onClick={changeThemeHandler} isDark={isDark} />
         </div>
