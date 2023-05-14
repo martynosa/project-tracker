f13ff225c2684caeee114db4161f9a09a924c78a
martynosa <martynosa@hotmail.com>
2023-05-14 14:45:30 +0300

    landing page done

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -11,35 +11,6 @@ const Landing = () => {
     navigate('/register');
   };
 
-  // return (
-  //   <section className={classes.container}>
-  //     <div>
-  //       <h1 className={classes.main}>
-  //         Reach new productivity<span>peaks</span>
-  //       </h1>
-
-  //       <p className={classes.secondary}>
-  //         Create, track and manage your projects with ease.
-  //       </p>
-
-  //       <div className={classes.btnBox}>
-  //         <Button color={'orange'} onClick={getStartedHandler}>
-  //           Get started
-  //           {arrowUpRightSVG}
-  //         </Button>
-  //       </div>
-  //     </div>
-
-  //     <div className={classes.imgBox}>
-  //       <img
-  //         className={classes.img}
-  //         src="../../imgs/landing-4.webp"
-  //         alt="people brainstorming"
-  //       />
-  //     </div>
-  //   </section>
-  // );
-
   return (
     <>
       <section className={classes.heroContainer}>
@@ -74,27 +45,39 @@ const Landing = () => {
         <div className={classes.features}>
           <h2>features</h2>
           <div className={classes.list}>
-            <div className={classes.card}>
+            <div className={`${classes.card} ${classes.blue}`}>
               <header className={classes.header}>
-                <h3 className={classes.title}>dummy title</h3>
+                <h3 className={classes.title}>Status-based organization</h3>
               </header>
-              <p className={classes.description}>dummy description</p>
+              <p className={classes.description}>
+                Keep track of your projects and their progress by easily
+                categorizing them into new, in progress, or completed.
+              </p>
             </div>
-            <div className={classes.card}>
+            <div className={`${classes.card} ${classes.orange}`}>
               <header className={classes.header}>
-                <h3 className={classes.title}>dummy title 2</h3>
+                <h3 className={classes.title}>Task management</h3>
               </header>
-              <p className={classes.description}>dummy description 2</p>
+              <p className={classes.description}>
+                Each project has its own subset of smaller tasks that you can
+                toggle to keep track of progress.
+              </p>
             </div>
-            <div className={classes.card}>
+            <div className={`${classes.card} ${classes.green}`}>
               <header className={classes.header}>
-                <h3 className={classes.title}>dummy title 3</h3>
+                <h3 className={classes.title}>
+                  Simple and intuitive interface
+                </h3>
               </header>
-              <p className={classes.description}>dummy description 3</p>
+              <p className={classes.description}>
+                Simple and intuitive interface that lets you focus on your work
+                without any distractions
+              </p>
             </div>
           </div>
         </div>
       </section>
+      <footer>Made by martynosa for demo purposes.</footer>
     </>
   );
 };
