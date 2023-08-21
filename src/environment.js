a6022966af98d25b9608a2fade70dda57fdcf4c1
martiN <78382341+martynosa@users.noreply.github.com>
2023-08-21 13:49:42 +0300

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -1,10 +1,12 @@
-const AUTH_URL = 'https://project-tracker-server.up.railway.app/auth';
-const PHOTO_URL = 'https://project-tracker-server.up.railway.app/users';
-const ITEM_URL = 'https://project-tracker-server.up.railway.app/items';
+const HOST = "https://project-tracker-server.onrender.com/";
 
-const DEV_AUTH_URL = 'http://localhost:5000/auth';
-const DEV_PHOTO_URL = 'http://localhost:5000/users';
-const DEV_ITEM_URL = 'http://localhost:5000/items';
+const AUTH_URL = `${HOST}/auth`;
+const PHOTO_URL = `${HOST}/users`;
+const ITEM_URL = `${HOST}/items`;
+
+const DEV_AUTH_URL = "http://localhost:5000/auth";
+const DEV_PHOTO_URL = "http://localhost:5000/users";
+const DEV_ITEM_URL = "http://localhost:5000/items";
 
 const URL = {
   AUTH_URL,
@@ -12,7 +14,7 @@ const URL = {
   ITEM_URL,
 };
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   URL.AUTH_URL = DEV_AUTH_URL;
   URL.PHOTO_URL = DEV_PHOTO_URL;
   URL.ITEM_URL = DEV_ITEM_URL;
