f50d54d55ce4c02f8704c79e012d4c8b06405805
martiN <78382341+martynosa@users.noreply.github.com>
2024-02-05 23:23:21 +0200

    on hover comment

--- src/Components/Common/Comment.js
+++ src/Components/Common/Comment.js
@@ -3,9 +3,8 @@ import classes from './Comment.module.css';
 
 const Comment = ({ children }) => {
   return (
-    <p className={classes.comment}>
+    <p className={classes.comment} title={children}>
       {largeExclamationMark}
-      {children}
     </p>
   );
 };
